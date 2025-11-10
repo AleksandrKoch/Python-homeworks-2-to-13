551e6eee7bdc223c2d631ce8a26a2030a4e4ca82
Alexander Koch <a_koch@apple.com>
2025-11-09 23:13:51 -0800

    HW2 is done

---
+++ lesson_2/practice_002_precedence.py
@@ -0,0 +1,14 @@
+# Try the following examples in Terminal and try to figure out
+# why the difference in results
+
+# >>> 4 + 100 / 10 * 5
+#54.0
+
+# >>> 4 + (100 / (10 * 5))
+#6.0
+
+# >>> (4 + 100 / 10) * 5
+#70.0
+
+# >>> ((4 + 100) / 10) * 5
+#52.0
---
+++ lesson_2/practice_004_variables_naming.py
@@ -0,0 +1,12 @@
+# Look at the precode and correct all invalid identifiers
+
+first_number = 34
+last_number= 42.67
+age = 25
+theanswer = 42
+Class = 1.8
+tax_rate = 0.07
+fav_colorcode = 4455
+shoeSize = 4
+
+# Send your code to the chat
