72c4e6f7b6cd6c643671a48f64b90b8e4b8bc560
Alexander Koch <a_koch@apple.com>
2025-11-26 22:59:06 -0800

    Lesson 7 task 6 done

---
+++ lesson_7/sandbox.py
@@ -0,0 +1,8 @@
+# Task 6. Using range() in a loop
+
+# Remember that range() returns an iterable, so you will usually find it used in a for loop.
+
+# Complete the following code so it prints the even numbers between 0 and 10;
+
+for i in range(0, 10, 2):
+    print(i)
