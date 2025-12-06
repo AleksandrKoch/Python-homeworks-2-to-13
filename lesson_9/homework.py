cfc52c4a4b9a03a030193953a2355d093a1a08f1
AleksandrKoch <kocherginalexander@gmail.com>
2025-12-05 17:17:57 -0800

    HW9 functions commit Dec 5 515PM

--- lesson_9/homework.py
+++ lesson_9/homework.py
@@ -56,10 +56,11 @@ def sum_of_two(num1=0, num2=0):
 
 # Prints: Nelson Mandela
 # full_name("Nelson", "Mandela")
-
+def full_name(first_name="Nelson", last_name="Mandela"):
+    print(first_name+", "+last_name)
 # Is there anything you can add to the line below, so the function also prints "Nelson Mandela"?
 # full_name("Mandela", "Nelson")
-
+full_name(last_name="Mandela", first_name="Nelson")
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Returning Values
@@ -68,7 +69,19 @@ def sum_of_two(num1=0, num2=0):
 # list using a for loop.
 
 # Tip: Validator functions return True / False which we can use in conditionals to do things like print a message.
+def validator(word):
+    if len(word) > 8:
+        return True
+    else:
+        return False
 
+list_of_words = ['1', '12', '123', '1234', '12345', '123456', '1234567', '12345678', '123456789', '']
+
+for word in list_of_words:
+    if validator(word) == False:
+        print(f"For word {word} length is less or equal to 8 symbols")
+    else:
+        print(f"For word {word} length is greater than 8 symbols")
 # ----------------------------------------------------------------------------------------------------------------------
 
 # You're going to revisit some of the algorithms you've already solved. But this time, there's a twist! Your challenge
--- lesson_9/sandbox.py
+++ lesson_9/sandbox.py
@@ -1,15 +1,20 @@
-# Multiple Parameters
-# Define a function that takes two parameters, add them up and prints the sum.
+# Returning Values
+# Define a validator function that you can use to determine if a word is longer than 8 characters.
+# After creating the function, make sure to test it. Create a list of words and iterate over this
+# list using a for loop.
 
-# Prints: The sum of 1 + 2 = 3
-# add(1, 2)
+# Tip: Validator functions return True / False which we can use in conditionals to do things like print a message.
 
-# Prints (default values might be useful): The sum of 1 + 0 = 1
-# add(1)
+def validator(word):
+    if len(word) > 8:
+        return True
+    else:
+        return False
 
-def sum_of_two(num1=0, num2=0):
-    print(f"The sum of {num1} + {num2} = {num1 + num2}")
-    return num1 + num2
+list_of_words = ['1', '12', '123', '1234', '12345', '123456', '1234567', '12345678', '123456789', '']
 
-sum_of_two(1)
-
+for word in list_of_words:
+    if validator(word) == False:
+        print(f"For word {word} length is less or equal to 8 symbols")
+    else:
+        print(f"For word {word} length is greater than 8 symbols")
